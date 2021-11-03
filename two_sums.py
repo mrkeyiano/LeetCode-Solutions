13b266db03b3cfd5615043b938cb7d53fa20d513
Femi Amoo <iamkeyiano@gmail.com>
2021-11-03 14:44:28 +0100

    Update two_sums.py

--- two_sums.py
+++ two_sums.py
@@ -1,6 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        solution_dict = {}
         for i, num in enumerate(nums):
             diff = target - num
-            if diff in nums:
+            if diff in nums and nums.index(diff) != i:
                 return nums.index(diff), i
